adb5f742e7fa4c8ad0f199c0365ceeb7b7f703af
AnjaliGhildiyal5 <ghildiyalanjali1985@gmail.com>
2026-06-23 23:22:24 +0530

    Update BasicWindow.java

--- BasicWindow.java
+++ BasicWindow.java
@@ -8,7 +8,7 @@ public class BasicWindow {
         JFrame frame = new JFrame("My First Swing Window");
 
         frame.setSize(400, 300);
-
+        frame.getContentPane().setBackground(Color.CYAN);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setVisible(true);
